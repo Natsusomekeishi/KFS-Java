7525d1ede80ab9ccc36c8a3332f6a47221117497
Natsusome Keishi <44630360+Natsusomekeishi@users.noreply.github.com>
2022-08-27 15:22:51 +0800

    更新主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,6 +12,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }]
     ],
+    theme: 'reco',
     themeConfig: {
         logo: '/favicon.ico',
         lastUpdated: '上次更新',
